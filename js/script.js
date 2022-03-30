fcaaa9bc7f6e84d72a3996ceb793564315d534fe
double <82323135+double-sucks@users.noreply.github.com>
2022-03-30 16:59:01 +0300

    force mime type

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+c function handleRequest(request) {
+    let resp = await fetch(request.url, request);
+  
+    let newResp = new Response(resp.body, {
+      headers: resp.headers,
+      status: resp.status
+    })
+  
+    if (request.url.endsWith(".css")) {
+      newResp.headers.set("Content-Type", "text/css");
+    }
+
+    if (request.url.endsWith(".js")) {
+        newResp.headers.set("Content-Type", "text/javascript");
+      }
+  
+    return newResp;
+  }
+  
+  addEventListener("fetch", event => event.respondWith(handleRequest(event.request)))
+
 const themeSwitch = document.querySelector('#themeSwitch')
 
 themeSwitch.addEventListener('change', () => {
